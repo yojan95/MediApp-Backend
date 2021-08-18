878daeb779e5494fd5403cfceb5f2f3c38e526f9
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 <estanfor95@gmail.com>
2021-08-18 09:53:11 -0500

    se añade anotacion service

--- src/main/java/com/alexis/service/impl/SignosServiceImpl.java
+++ src/main/java/com/alexis/service/impl/SignosServiceImpl.java
@@ -1,12 +1,14 @@
 package com.alexis.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.alexis.model.Signos;
 import com.alexis.repo.IGenericRepo;
 import com.alexis.repo.ISignosRepo;
 import com.alexis.service.ISignosService;
 
+@Service
 public class SignosServiceImpl  extends CRUDImpl<Signos, Integer> implements ISignosService{
 	
 	@Autowired
