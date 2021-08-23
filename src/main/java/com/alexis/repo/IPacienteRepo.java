dbdc72667124627513ef3af0c5bfafde91fae785
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 <estanfor95@gmail.com>
2021-08-23 09:50:20 -0500

    se añade consulta para obtener id por dni paciente

--- src/main/java/com/alexis/repo/IPacienteRepo.java
+++ src/main/java/com/alexis/repo/IPacienteRepo.java
@@ -1,9 +1,17 @@
 package com.alexis.repo;
 
+
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+
 import com.alexis.model.Paciente;
 
 public interface IPacienteRepo extends IGenericRepo<Paciente, Integer>{
 	
 	
-
+	@Query(value = "select p.id_paciente from paciente p where p.dni = :dni", nativeQuery = true)
+	Object buscarDni(String dni);
+	
 }
